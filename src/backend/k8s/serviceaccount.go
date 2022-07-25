eb00ba7158538df81a24ce901f200b98b1d269c9
Jan Lauber <jan.lauber@protonmail.ch>
2022-07-25 19:03:39 +0200

    renaming and breaking stuff v2
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- src/backend/k8s/serviceaccount.go
+++ src/backend/k8s/serviceaccount.go
@@ -1,6 +1,6 @@
 package k8s
 
-import "github.com/natrongmbh/kubeperm/models"
+import "github.com/natrongmbh/kubeidentity/models"
 
 func GetMatchingServiceAccountsByGithubTeam(githubUser models.GithubUser) ([]string, error) {
 	return []string{}, nil
