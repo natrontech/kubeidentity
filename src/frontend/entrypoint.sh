dc87eb48216fd01fa62b8d1f16f7862e87b54ac9
Jan Lauber <jan.lauber@protonmail.ch>
2022-08-31 22:30:44 +0200

    fix entrypoint script
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- src/frontend/entrypoint.sh
+++ src/frontend/entrypoint.sh
@@ -6,19 +6,31 @@ envFilename='.env.production'
 nextFolder='./.next/'
 function apply_path {
 
-    env | grep ENV_ | while read -r line; do
-        # split the variable into name and value
-        configName=$(echo $line | cut -d'=' -f1)
-        configValue=$(echo $line | cut -d'=' -f2)
+    # get every env variable by line and check if it starts with ENV_
+    envvars=$(env | grep '^ENV_')
+
+    # if there is no env variable, exit
+    if [ -z "$envvars" ]; then
+        echo "No env variables found"
+        exit 0
+    fi
+
+    # echo every envvars
+    echo "Env variables found:"
+    while read -r line; do
+        configName=$(echo "$line" | cut -d'=' -f1)
+        configValue=$(echo "$line" | cut -d'=' -f2)
 
         envValue=$(env | grep "^$configName=" | grep -oe '[^=]*$')
-        # if config found
+
         if [ -n "$configValue" ] && [ -n "$envValue" ]; then
-            # replace all
-            echo "Replace: ${configValue} with: ${envValue}"
+            echo "Setting $configName to $configValue"
+            # sed -i "s/$configName=.*/$configName=$configValue/g" $envFilename
             find $nextFolder \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#$configValue#$envValue#g"
         fi
-    done <$envFilename
+
+    done <<< "$envvars"
+
 }
 apply_path
 echo "Starting Nextjs"
